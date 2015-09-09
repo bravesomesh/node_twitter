e239e1b8fa8bf7e861346c57bcf891d6d0efa173
Somesh Vyas <somesh.vyas@bookmyshow.com>
2015-09-09 13:07:07 +0530

    changed html file structure

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules'));
 
 app.get("/", function(req, res){
-    // res.render("page.html");
-	res.render("index.html");    
+    res.render("index.html");
 });
 
 var twitter = require('./config/twitter');
